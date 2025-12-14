bf7ecf2cc89a0d028af6a889887dae0a72e85c24
daellada <daellada@student.21-school.ru>
2025-12-14 15:57:19 +0300

    Add provision.sh to configure Vagrant machines

---
+++ vagrant_VM/provision.sh
@@ -0,0 +1,18 @@
+apt-get update
+apt-get -y upgrade
+apt-add-repository ppa:ondrej/php -y
+apt-get update
+apt-get install -y software-properties-common curl zip
+
+#!/usr/bin/env bash
+set -e
+HOSTS=(
+    "192.168.88.10  course-1"
+    "192.168.88.11  course-2"
+)
+
+for LINE in "${HOSTS[@]}"; do
+  if ! grep -qF "$LINE" /etc/hosts; then
+    echo "$LINE" | sudo tee -a /etc/hosts > /dev/null
+  fi
+done
